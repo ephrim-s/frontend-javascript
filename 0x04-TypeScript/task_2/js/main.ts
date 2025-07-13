01ee98a58305344f607b1317f09e16f81a29ad16
ephrim-s <ephrims5@gmial.com>
2025-07-13 20:25:57 +0000

    updated

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,4 +1,3 @@
-
 interface DirectorInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
@@ -46,6 +45,31 @@ function createEmployee(salary: number | string): Director | Teacher {
     return new Director();
 }
 
-console.log(createEmployee(200));    // Teacher
-console.log(createEmployee(1000));   // Director
-console.log(createEmployee('$500')); // Director
+function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+console.log(executeWork(createEmployee(200)));    // Getting to work
+console.log(executeWork(createEmployee(1000)));   // Getting to director tasks
+
+
+// String literal types
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    }
+    return 'Teaching History';
+}
+
+console.log(teachClass('Math'));    // Teaching Math
+console.log(teachClass('History')); // Teaching History
